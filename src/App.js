80443d638130f2433152a0120e8401e5d9c6209e
DevPed <pedromfr91@gmail.com>
2022-09-26 07:46:02 +0200

    delete assets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-import Dark from './components/background/dark/Dark';
-import Waves from './components/background/waves/Waves';
+
 import Container from './components/container/Container';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
